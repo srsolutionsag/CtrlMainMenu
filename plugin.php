dd505f5b8844a7e4228a39e9da25a6652e5f39a8
Fabian Schmid <fs@studer-raimann.ch>
2016-03-14 17:36:23 +0100

    updated supported versions

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id = 'ctrlmm';
 $version = '3.2.04';
-$ilias_min_version = '4.4.000';
-$ilias_max_version = '5.1.999';
+$ilias_min_version = '5.0.000';
+$ilias_max_version = '5.2.999';
 $responsible = 'Fabian Schmid';
 $responsible_mail = 'fs@studer-raimann.ch';
 ?>

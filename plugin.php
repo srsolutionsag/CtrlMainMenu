584af9e513350a52411b792cdb7863c6936b1379
Florian <fw@studer-raimann.ch>
2018-09-12 08:58:58 +0200

    Fix support email

--- plugin.php
+++ plugin.php
@@ -4,4 +4,4 @@ $version = '3.6.0';
 $ilias_min_version = '5.2.000';
 $ilias_max_version = '5.3.999';
 $responsible = 'studer + raimann ag';
-$responsible_mail = 'support@studer-raimann.ch';
+$responsible_mail = 'support-core1@studer-raimann.ch';

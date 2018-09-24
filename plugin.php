64902cc798a574161dcf490daa080ae1a23e926e
Fabian Schmid <fs@studer-raimann.ch>
2018-09-24 10:09:41 +0200

    updated plugin version

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 $id = 'ctrlmm';
-$version = '3.6.0';
+$version = '3.6.1';
 $ilias_min_version = '5.2.000';
 $ilias_max_version = '5.3.999';
 $responsible = 'studer + raimann ag - Team Core 1';

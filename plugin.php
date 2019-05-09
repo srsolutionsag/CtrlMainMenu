97c4e029d16bc03a8176179533a84db881260c11
sragfw <fw@studer-raimann.ch>
2019-05-09 10:21:26 +0200

    Only ILIAS 5.4

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'ctrlmm';
 $version = '3.6.2';
-$ilias_min_version = '5.3.000';
+$ilias_min_version = '5.4.000';
 $ilias_max_version = '5.4.999';
 $responsible = 'studer + raimann ag - Team Core';
 $responsible_mail = 'support-core1@studer-raimann.ch';
